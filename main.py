dcdfb90da677716aec9b700caa6d736456600bc9
unknown <marcin.migal@edu.uekat.pl>
2022-04-03 08:36:17 +0200

    Initial Commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+from cleaning import text_cleaner
+from stemming import stem_text
+
+
+text_list = []
+
+with open('true.csv', 'r', encoding='utf-8') as file:
+    for  i, row  in enumerate(file.readlines()):
+        line = text_cleaner(row)
+        line = stem_text(line)
+        text_list.extend(line)
+        # print(i)
+        # if i >=50:
+        #     break
+
+bow = dict([[x,text_list.count(x)] for x in set(text_list)])
+
+
+
+
+
+wc = WordCloud()
+wc.generate_from_frequencies(bow)
+plt.imshow(wc, interpolation='bilinear')
+plt.axis("off")
+plt.show()
