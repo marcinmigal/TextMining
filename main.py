f65c07f10ffc9bb0ecf970c19ee0406758100d81
unknown <marcin.migal@edu.uekat.pl>
2022-05-08 08:49:05 +0200

    Next Commit

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+
 from prettytable import PrettyTable
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
 
 from cleaning import text_cleaner
 from stemming import stem_text
-plt.style.use('seaborn-dark-palette')
+
+plt.style.use('seaborn-dark')
+
 
 def text_tokenizer(text):
     text = text_cleaner(text)
@@ -24,28 +29,8 @@ mail_data.loc[mail_data['Category'] == 'spam', 'Category',] = 0
 mail_data.loc[mail_data['Category'] == 'ham', 'Category',] = 1
 
 
-X_df = mail_data['Message']
-Y_df = mail_data['Category']
-
-
-X_train, X_test, Y_train, Y_test = train_test_split(X_df, Y_df, test_size=0.2, random_state=3)
-
-feature_extraction = TfidfVectorizer(min_df = 1, stop_words='english', lowercase='True')
-# feature_extraction = TfidfVectorizer(tokenizer=text_tokenizer)
-
-
-
-
-# Wyciągnięcie tokenów dla wiadomości oznaczonych jako spam
- 
-# arr = feature_extraction.fit_transform(mail_data[mail_data['Category']==0]['Message'])
-# arr = arr.toarray()
-
-# tfid_features = feature_extraction.get_feature_names_out(mail_data[mail_data['Category']==0]['Message'])
-
-# print("\nNajwazniejsze tokeny: ")
-# for i in arr[0].argsort()[:10]:
-#     print(tfid_features[i])
+# feature_extraction = TfidfVectorizer(min_df = 1, stop_words='english', lowercase='True')
+feature_extraction = TfidfVectorizer(tokenizer=text_tokenizer)
 
 
 count_vectorizer = CountVectorizer(min_df = 1, stop_words='english', lowercase='True')
@@ -53,13 +38,6 @@ X_transform = count_vectorizer.fit_transform(mail_data[mail_data['Category']==0]
 arr = X_transform.toarray()
 
 count_features = count_vectorizer.get_feature_names_out(mail_data[mail_data['Category']==0]['Message'])
-
-print("Najczesciej wystepujace tokeny: ")
-for i in arr[0].argsort()[:10]:
-    print(count_features[i])
-
-
-
 
 sum_words = X_transform.sum(axis=0) 
 words_freq = [(word, sum_words[0, idx]) for word, idx in  count_vectorizer.vocabulary_.items()]
@@ -69,35 +47,34 @@ n_words = 15
 top_n_words = list(words_freq.keys())[:n_words]
 top_n_words_count = list(words_freq.values())[:n_words]
 
+
 plt.subplots(figsize=(11, 5))
 plt.xlabel("Term")
 plt.ylabel("Count")
-plt.title("Count")
+plt.title("Count of most common words")
 plt.bar(top_n_words,top_n_words_count)
 plt.show()
 
 
 pretty_table = PrettyTable()
-pretty_table.title = "Count"
+pretty_table.title = "Count of most common words"
 pretty_table.add_column("Term", top_n_words[:10])
 pretty_table.add_column("Count", top_n_words_count[:10])
+pretty_table
 
 
+X_df = mail_data['Message']
+Y_df = mail_data['Category']
 
+
+X_train, X_test, Y_train, Y_test = train_test_split(X_df, Y_df, test_size=0.2, random_state=3)
 
 
 X_train_features = feature_extraction.fit_transform(X_train)
 X_test_features = feature_extraction.transform(X_test)
 
-Y_train = Y_train.astype('int')
-Y_test = Y_test.astype('int')
-
-
-print(X_train)
-
-print(X_train_features)
-
-
+Y_train = Y_train.astype(int)
+Y_test = Y_test.astype(int)
 
 
 # Tworzenie i uczenie modelu
@@ -108,13 +85,26 @@ model.fit(X_train_features, Y_train)
 prediction_on_training_data = model.predict(X_train_features)
 accuracy_on_training_data = accuracy_score(Y_train, prediction_on_training_data)
 
-print('Accuracy on training data : ', accuracy_on_training_data)
+print('Training data accuracy: ', accuracy_on_training_data)
 
 prediction_on_test_data = model.predict(X_test_features)
 accuracy_on_test_data = accuracy_score(Y_test, prediction_on_test_data)
-print('Accuracy on test data : ', accuracy_on_test_data)
+print('Test data accuracy: ', accuracy_on_test_data)
 
-
+#False Posives Testing
+logit_roc_auc = roc_auc_score(Y_test, model.predict(X_test_features))
+fpr, tpr, thresholds = roc_curve(Y_test, model.predict_proba(X_test_features)[:,1])
+plt.figure()
+plt.plot(fpr, tpr, label='Logistic Regression (area = %0.2f)' % logit_roc_auc)
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic')
+plt.legend(loc="lower right")
+plt.savefig('Log_ROC')
+plt.show()
 
 # Testowanie modelu
 input_mail = ["I've been searching for the right words to thank you for this breather. I promise i wont take your help for granted and will fulfil my promise. You have been wonderful and a blessing at all times"]
